3eef62cd450265e599b4b61547cbd1c89b0101cb
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-12 00:07:29 -0200

    Add Linux clang build
    In the shell script, the CMake CXX flags are updated to use
    libc++ (for linking) when compiling with clang++. This leaves an
    issue of duplicate flag configuration, one in the CMakeLists and
    the other on the bash shell for Travis. Future maintenance or
    refacot should address this issue if possible.

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -2,4 +2,8 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo -E apt-get update -qq
+
+  if [ "$COMPILER" == "clang++" ]; then
+    sudo apt-get -y install libc++-dev
+  fi
 fi
--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -2,6 +2,12 @@
 
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=$COMPILER ..
+
+if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$COMPILER" == "clang++" ]; then
+  cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_CXX_FLAGS="-Wall -stdlib=libc++ -std=c++14" ..
+else
+  cmake -DCMAKE_CXX_COMPILER=$COMPILER ..
+fi
+
 make
 make test CTEST_OUTPUT_ON_FAILURE=TRUE
