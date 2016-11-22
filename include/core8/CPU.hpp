9349b371acca60dacadfb2efa13ad82f1f06cab2
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-22 21:43:42 -0200

    Add dispatch table to CPU

--- include/core8/CPU.hpp
+++ include/core8/CPU.hpp
@@ -2,6 +2,8 @@
 #define CORE8_CPU_HPP
 
 #include <array>
+#include <functional>
+#include <map>
 
 #include "Chip8.hpp"
 
@@ -9,13 +11,17 @@ namespace Core8 {
 
 class CPU {
   public:
-    CPU() = default;
+    CPU();
 
     Chip8::BYTE readRegister(const Chip8::REGISTER id) const;
     void writeRegister(const Chip8::REGISTER id, const Chip8::BYTE value);
 
   private:
+    void loadNnToVx();
+    void addNnToVx();
+
     std::array<Chip8::BYTE, Chip8::NUMBER_OF_REGISTERS> registers;
+    const std::map<Chip8::OPCODE, std::function<void(void)>> dispatchTable;
 };
 
 } // namespace Core8
--- src/CPU.cpp
+++ src/CPU.cpp
@@ -2,12 +2,26 @@
 
 namespace Core8 {
 
+CPU::CPU()
+: dispatchTable{
+    {Chip8::OPCODE::LOAD_NN_TO_VX, [this] () { loadNnToVx(); }},
+    {Chip8::OPCODE::ADD_NN_TO_VX, [this] () { addNnToVx(); }}
+  }
+{
+}
+
 Chip8::BYTE CPU::readRegister(const Chip8::REGISTER id) const {
   return registers[static_cast<std::size_t>(id)];
 }
 
 void CPU::writeRegister(const Chip8::REGISTER id, const Chip8::BYTE value) {
   registers[static_cast<std::size_t>(id)] = value;
+}
+
+void CPU::loadNnToVx() {
+}
+
+void CPU::addNnToVx() {
 }
 
 } //namespace Core8
