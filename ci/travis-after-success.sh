1fac3ecd61050bd685bca5327a03d1aa89a6b978
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2017-01-20 22:43:00 -0200

    Exclude test and CMake folders from coverage

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [ "$IS_COVERAGE_BUILD" == 1 ]; then
-  coveralls --exclude externals --gcov-options '\-lp'
+  coveralls --exclude externals --exclude test --exclude CMakeFiles --gcov-options '\-lp'
 fi
