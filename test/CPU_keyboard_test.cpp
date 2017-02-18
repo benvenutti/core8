586e99d24b632a7be9d7406e13f9f044ffe6911f
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2017-02-18 16:25:17 -0200

    Create tests for EX9E operation

---
+++ test/CPU_keyboard_test.cpp
@@ -0,0 +1,56 @@
+#include <catch.hpp>
+
+#include "aux/Aux.hpp"
+#include "CPU.hpp"
+
+namespace {
+
+struct CpuFixture {
+  Aux::TestKit testKit;
+  Core8::CPU& cpu = testKit.cpu;
+  Aux::IoConnectorMock& ioConnector = testKit.ioConnector;
+};
+
+SCENARIO_METHOD(
+    CpuFixture,
+    "Operation EX9E skips next instruction when VX is pressed", "[keyboard]"
+) {
+  GIVEN("A CPU with V0 set to 0 and an i/o connector with the key 0 pressed") {
+    cpu.writeRegister(Core8::Chip8::REGISTER::V0, 0u);
+    ioConnector.setPressedKey(Core8::Chip8::KEY::K0);
+    const auto originalPc = cpu.getPc();
+
+    WHEN("the CPU executes a EX9E operation with X equal to 0") {
+      cpu.setInstruction(0xE09E);
+      cpu.decode();
+      cpu.execute();
+
+      THEN("the program counter is updated to skip the next instruction") {
+        REQUIRE(cpu.getPc() == (originalPc + Core8::Chip8::INSTRUCTION_BYTE_SIZE));
+      }
+    }
+  }
+}
+
+SCENARIO_METHOD(
+    CpuFixture,
+    "Operation EX9E does not skip instruction when VX is not pressed", "[keyboard]"
+) {
+  GIVEN("A CPU with V0 set to 0 and an i/o connector with the key 1 pressed") {
+    cpu.writeRegister(Core8::Chip8::REGISTER::V0, 0u);
+    ioConnector.setPressedKey(Core8::Chip8::KEY::K1);
+    const auto originalPc = cpu.getPc();
+
+    WHEN("the CPU executes a EX9E operation with X equal to 0") {
+      cpu.setInstruction(0xE09E);
+      cpu.decode();
+      cpu.execute();
+
+      THEN("the program counter remains unchanged") {
+        REQUIRE(cpu.getPc() == originalPc);
+      }
+    }
+  }
+}
+
+} // unnamed namespace
--- test/aux/IoConnectorMock.hpp
+++ test/aux/IoConnectorMock.hpp
@@ -12,13 +12,18 @@ class IoConnectorMock : public Core8::IoConnector {
   public:
     virtual void drawScreen(
         const std::array<Core8::Chip8::BYTE, Core8::Chip8::DISPLAY_SIZE>& /*frame*/
-    ) override {
+    ) override { }
 
+    virtual bool isKeyPressed(const Core8::Chip8::KEY key) const override {
+      return key == pressedKey;
     }
 
-    virtual bool isKeyPressed(const Core8::Chip8::KEY /*key*/) const override {
-
+    void setPressedKey(const Core8::Chip8::KEY key) {
+      pressedKey = key;
     }
+
+  private:
+    Core8::Chip8::KEY pressedKey{Core8::Chip8::KEY::NONE};
 };
 
 } // namespace Aux
