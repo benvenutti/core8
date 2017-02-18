e66f3a78eb7b9f0900e1121331d0f2852027d623
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2017-02-18 16:30:34 -0200

    Create tests for EXA1 operation

--- test/CPU_keyboard_test.cpp
+++ test/CPU_keyboard_test.cpp
@@ -20,7 +20,7 @@ SCENARIO_METHOD(
     ioConnector.setPressedKey(Core8::Chip8::KEY::K0);
     const auto originalPc = cpu.getPc();
 
-    WHEN("the CPU executes a EX9E operation with X equal to 0") {
+    WHEN("the CPU executes an EX9E operation with X equal to 0") {
       cpu.setInstruction(0xE09E);
       cpu.decode();
       cpu.execute();
@@ -41,12 +41,54 @@ SCENARIO_METHOD(
     ioConnector.setPressedKey(Core8::Chip8::KEY::K1);
     const auto originalPc = cpu.getPc();
 
-    WHEN("the CPU executes a EX9E operation with X equal to 0") {
+    WHEN("the CPU executes an EX9E operation with X equal to 0") {
       cpu.setInstruction(0xE09E);
       cpu.decode();
       cpu.execute();
 
       THEN("the program counter remains unchanged") {
+        REQUIRE(cpu.getPc() == originalPc);
+      }
+    }
+  }
+}
+
+SCENARIO_METHOD(
+    CpuFixture,
+    "Operation EXA1 skips next instruction when VX is not pressed", "[keyboard]"
+) {
+  GIVEN("A CPU with V0 set to 1 and an i/o connector with the key 0 pressed") {
+    cpu.writeRegister(Core8::Chip8::REGISTER::V0, 1u);
+    ioConnector.setPressedKey(Core8::Chip8::KEY::K0);
+    const auto originalPc = cpu.getPc();
+
+    WHEN("the CPU executes an EXA1 operation with X equal to 0") {
+      cpu.setInstruction(0xE0A1);
+      cpu.decode();
+      cpu.execute();
+
+      THEN("the program counter is updated to skip the next instruction") {
+        REQUIRE(cpu.getPc() == (originalPc + Core8::Chip8::INSTRUCTION_BYTE_SIZE));
+      }
+    }
+  }
+}
+
+SCENARIO_METHOD(
+    CpuFixture,
+    "Operation EXA1 does not skip instruction when VX is pressed", "[keyboard]"
+) {
+  GIVEN("A CPU with V0 set to 0 and an i/o connector with the key 0 pressed") {
+    cpu.writeRegister(Core8::Chip8::REGISTER::V0, 0u);
+    ioConnector.setPressedKey(Core8::Chip8::KEY::K0);
+    const auto originalPc = cpu.getPc();
+
+    WHEN("the CPU executes an EXA1 operation with X equal to 0") {
+      cpu.setInstruction(0xE0A1);
+      cpu.decode();
+      cpu.execute();
+
+      THEN("the program counter is updated to skip the next instruction") {
         REQUIRE(cpu.getPc() == originalPc);
       }
     }
