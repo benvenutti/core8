5fa27829d74cfbaa8597f6b6fe9b44155c27c3bb
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2017-02-20 17:35:49 -0300

    Refactor Travis CI scripts for compiler flags

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -9,9 +9,9 @@ if [ "$IS_COVERAGE_BUILD" == 1 ]; then
   flags="-g -O0 --coverage"
   cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="$flags" -DCMAKE_CXX_COMPILER=$COMPILER .
 elif [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$COMPILER" == "clang++" ]; then
-  cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_CXX_FLAGS="-Wall -stdlib=libc++ -std=c++14" ..
+  cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_CXX_FLAGS="-stdlib=libc++" ..
 elif [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$COMPILER" == "g++-4.9" ]; then
-  cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_CXX_FLAGS="-Wall -std=c++1y" ..
+  cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_CXX_FLAGS="-std=c++1y" ..
 else
   cmake -DCMAKE_CXX_COMPILER=$COMPILER ..
 fi
