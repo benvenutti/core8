b7772199012bba50473cc3813848023533fd2c33
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2017-03-05 15:22:51 -0300

    Drop clang 3.5

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -2,12 +2,6 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo -E apt-get update -qq
-
-  if [ "$COMPILER" == "clang++" ]; then
-    sudo apt-get -y install libc++-dev
-  elif [ "$COMPILER" == "clang++-3.7" ]; then
-    sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.7 60 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-3.7
-  fi
 fi
 
 if [ "$IS_COVERAGE_BUILD" == 1 ]; then
