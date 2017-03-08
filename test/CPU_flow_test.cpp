bd6e69bca5ff004f6ff46ff6f37ef2eb8f62dc57
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2017-03-08 20:00:35 -0300

    Refactor flow tests

--- test/CPU_flow_test.cpp
+++ test/CPU_flow_test.cpp
@@ -6,44 +6,43 @@
 
 namespace {
 
-using namespace Core8;
+struct CpuFixture {
+  Aux::TestKit testKit;
+  Core8::CPU& cpu = testKit.cpu;
+};
 
-SCENARIO("CPUs can execute unconditional jumps", "[flow]") {
+SCENARIO_METHOD(
+    CpuFixture,
+    "CPU executes an unconditional jump to address NNN with opcode 1NNN",
+    "[flow]"
+) {
   GIVEN("A CPU") {
-    Aux::TestKit testKit;
-    CPU& cpu = testKit.cpu;
-
-    WHEN("the CPU executes an 1NNN operation") {
+    WHEN("the CPU executes an 1NNN opcode") {
       cpu.execute(0x1ABC);
-      const auto pc1 = cpu.getPc();
-
-      cpu.execute(0x10D2);
-      const auto pc2 = cpu.getPc();
 
       THEN("the program counter is updated to the value of NNN") {
-        REQUIRE(pc1 == 0xABC);
-        REQUIRE(pc2 == 0x0D2);
+        REQUIRE(cpu.getPc() == 0xABC);
       }
     }
   }
 }
 
-SCENARIO("CPUs can call subroutines", "[flow]") {
+SCENARIO_METHOD(
+    CpuFixture,
+    "CPU calls a subroutine with opcode 2NNN",
+    "[flow]"
+) {
   GIVEN("A CPU") {
-    Aux::TestKit testKit;
-    CPU& cpu = testKit.cpu;
-
-    WHEN("the CPU calls a subroutine executing an 2NNN operation") {
-      const auto pc = cpu.getPc();
-      const auto sp = cpu.getSp();
-
+    WHEN("the CPU calls a subroutine executing a 2NNN opcode") {
+      const auto originalPc = cpu.getPc();
+      const auto originalSp = cpu.getSp();
       cpu.execute(0x2656);
 
       THEN("the program counter is pushed to the call stack") {
-        REQUIRE(cpu.getStack().at(sp) == pc);
+        REQUIRE(cpu.getStack().at(originalSp) == originalPc);
       }
       AND_THEN("the stack pointer is incremented") {
-        REQUIRE(cpu.getSp() == sp + 1);
+        REQUIRE(cpu.getSp() == originalSp + 1u);
       }
       AND_THEN("the program counter is updated to the value of NNN") {
         REQUIRE(cpu.getPc() == 0x656);
@@ -52,33 +51,37 @@ SCENARIO("CPUs can call subroutines", "[flow]") {
   }
 }
 
-SCENARIO("CPUs can return from subroutines", "[flow]") {
+SCENARIO_METHOD(
+    CpuFixture,
+    "CPU returns from subroutine with opcode 00EE",
+    "[flow]"
+) {
   GIVEN("A CPU executing a subroutine") {
-    Aux::TestKit testKit;
-    CPU& cpu = testKit.cpu;
     cpu.execute(0x2ABC);
-
     const auto sp = cpu.getSp();
-    const auto stackTop = cpu.getStack().at(sp - 1u);
+    const auto previousAddress = cpu.getStack().at(sp - 1u);
 
     WHEN("the CPU executes a 00EE operation to return from a subroutine") {
       cpu.execute(0x00EE);
 
       THEN("the call stack topmost value is assigned to the program counter") {
-        REQUIRE(cpu.getPc() == stackTop);
+        REQUIRE(cpu.getPc() == previousAddress);
       }
       AND_THEN("the stack pointer is decremented") {
-        REQUIRE(cpu.getSp() == sp - 1);
+        REQUIRE(cpu.getSp() == sp - 1u);
       }
     }
   }
 }
 
-SCENARIO("CPUs can execute unconditional jumps using register V0", "[flow]") {
-  GIVEN("A CPU with initialized registers") {
-    Aux::TestKit testKit;
-    CPU& cpu = testKit.cpu;
-    cpu.writeRegister(Chip8::Register::V0, 0x2D);
+SCENARIO_METHOD(
+    CpuFixture,
+    "CPU executes an unconditional jump with opcode BNNN"
+        "to address NNN plus register V0",
+    "[flow]"
+) {
+  GIVEN("A CPU with initialized register") {
+    cpu.writeRegister(Core8::Chip8::Register::V0, 0x2D);
 
     WHEN("the CPU executes an BNNN operation") {
       cpu.execute(0xB131);
@@ -90,4 +93,4 @@ SCENARIO("CPUs can execute unconditional jumps using register V0", "[flow]") {
   }
 }
 
-} // unnamed namespace
+} // namespace
