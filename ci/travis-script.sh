33da4b002fdce9fd77cd5cefb5c93cb85c4ab6e5
Diogo Benvenutti <diogo.benvenutti@native-instruments.de>
2018-05-11 00:37:46 +0200

    Fix CMAKE_PREFIX_PATH for Qt on OSX

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -8,8 +8,10 @@ fi
 if [ "$IS_COVERAGE_BUILD" == 1 ]; then
   flags="-g -O0 --coverage"
   cmake -DCMAKE_C_COMPILER=$COMPILER_CC -DCMAKE_CXX_COMPILER=$COMPILER_CXX -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="$flags" .
-else
+elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
   cmake -DCMAKE_C_COMPILER=$COMPILER_CC -DCMAKE_CXX_COMPILER=$COMPILER_CXX ..
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  cmake -DCMAKE_C_COMPILER=$COMPILER_CC -DCMAKE_CXX_COMPILER=$COMPILER_CXX -DCMAKE_PREFIX_PATH="`brew --prefix qt`" ..
 fi
 
 make
