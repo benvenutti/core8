1d5341befb07e59eb7a09aca1f22fd224654d062
benvenutti <diogo.a.benvenutti@gmail.com>
2018-10-21 17:28:18 +0200

    Force prefix path on OSX

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -2,7 +2,13 @@
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_PREFIX_PATH="$(brew --prefix qt)" ..
+fi
+
 cat compile_commands.json
 cmake --build . -- -j2
 ctest -VV .
