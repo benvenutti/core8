cf873113ce5ab5f4cc709c527173a341e6a74391
benvenutti <diogo.a.benvenutti@gmail.com>
2018-10-21 18:25:18 +0200

    Add CodeCov script

--- ci/travis-coverage.sh
+++ ci/travis-coverage.sh
@@ -2,7 +2,7 @@
 
 if [ "$BUILD_TYPE" == "Coverage" ]; then
   lcov --gcov-tool $(which ${GCOV}) -d . -c -o coverage.info
-  lcov --gcov-tool $(which ${GCOV}) -r coverage.info '/usr/*' 'model/tests/*' 'libs/*' -o coverage.info
+  lcov --gcov-tool $(which ${GCOV}) -r coverage.info '/usr/*' 'libs/*' 'model/tests/*' -o coverage.info
   lcov --gcov-tool $(which ${GCOV}) -l coverage.info
   bash <(curl -s https://codecov.io/bash) -X gcov || echo "Codecov did not collect coverage reports"
 fi
